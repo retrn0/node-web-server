6bed57646cfe9ab43a418170d259d9b588a2bf11
Kedar Joshi <kedarjo@gmail.com>
2018-06-10 13:04:17 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamWords', text => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  let log = `${new Date().toString()} ${req.method} ${req.path} \n`;
+  fs.appendFile('server.log', log, error => {
+    console.log('something went wrong yo!');
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'This is a home page',
+    welcomeMsg: 'Welcome to my world'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'uh oh something went wrong'
+  });
+});
+app.listen(port, () => console.log(`server is up on port ${port}`));
